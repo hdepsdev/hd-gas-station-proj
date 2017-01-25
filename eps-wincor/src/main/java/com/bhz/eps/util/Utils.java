4283b46d72c0abc9f812160ddd52eaa069aec48f
hdepsdev <1519166445@qq.com>
2017-01-25 13:27:11 +0800

    Add UUID Generator

--- eps-wincor/src/main/java/com/bhz/eps/util/Utils.java
+++ eps-wincor/src/main/java/com/bhz/eps/util/Utils.java
@@ -24,6 +24,22 @@ public class Utils {
 		}
 	}
 	
+	public static String generateUUID(String sep){
+		if(sep!=null){
+			return com.fasterxml.uuid.Generators.randomBasedGenerator(new java.util.Random()).generate().toString().replace("-", sep);
+		}else{
+			return com.fasterxml.uuid.Generators.randomBasedGenerator(new java.util.Random()).generate().toString();
+		}
+	}
+	
+	public static String generateCompactUUID(){
+		return generateUUID("");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(generateCompactUUID());
+	}
+	
 	// UUID Dictionary (Alpha + Number)
 	private final static String[] chars = new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
 			"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8",
