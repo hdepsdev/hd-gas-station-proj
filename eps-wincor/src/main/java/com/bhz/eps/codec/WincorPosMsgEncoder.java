9270356b34bea97d1394d0680fce752788de8f6c
yangxb2 <yangxb@bhz.com.cn>
2017-01-25 10:39:07 +0800

    add saleitem object and its dao, service & service impl

--- eps-wincor/src/main/java/com/bhz/eps/codec/WincorPosMsgEncoder.java
+++ eps-wincor/src/main/java/com/bhz/eps/codec/WincorPosMsgEncoder.java
@@ -1,11 +1,25 @@
 package com.bhz.eps.codec;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.bhz.eps.entity.CardServiceResponse;
+import com.thoughtworks.xstream.XStream;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
 public class WincorPosMsgEncoder extends MessageToByteEncoder<Object> {
-
+	
+	private static XStream xstream;
+	static {
+		xstream = new XStream();
+		xstream.autodetectAnnotations(true);
+		xstream.ignoreUnknownElements();
+	}
+	
+	private static final Logger logger = LogManager.getLogger(WincorPosMsgDecoder.class);
 
     public WincorPosMsgEncoder() {
     	
@@ -13,7 +27,19 @@ public class WincorPosMsgEncoder extends MessageToByteEncoder<Object> {
 
     @Override
     protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
+    	CardServiceResponse response = (CardServiceResponse) msg;
     	
+    	String content = xstream.toXML(response);
     	
+    	logger.info(content);
+    	
+    	byte[] lv = new byte[content.length() + 4];
+    	lv[0] = (byte)(content.length()>>24);
+    	lv[1] = (byte)(content.length()>>16);
+    	lv[2] = (byte)(content.length()>>8);
+    	lv[3] = (byte)(content.length()&0xFF);
+    	System.arraycopy(content.getBytes("UTF-8"), 0, lv, 4, content.length());
+    	
+    	out.writeBytes(lv);
     }
 }
