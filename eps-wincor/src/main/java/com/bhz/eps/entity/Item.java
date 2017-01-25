97ee67e2ca02ba195deae57b5e870004ddc8793b
yangxb2 <yangxb@bhz.com.cn>
2017-01-25 11:02:51 +0800

    add item entity, dao, service, service.impl

---
+++ eps-wincor/src/main/java/com/bhz/eps/entity/Item.java
@@ -0,0 +1,18 @@
+package com.bhz.eps.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Item implements java.io.Serializable{
+	private static final long serialVersionUID = 253906985343295769L;
+	
+	@Getter@Setter
+	private String code;
+	
+	@Getter@Setter
+	private String name;
+	
+	@Getter@Setter
+	private int catalog;
+
+}
